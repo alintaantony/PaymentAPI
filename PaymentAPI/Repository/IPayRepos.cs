c9b6425e20db56166c220b65098f78494e7b24d6
Alint Antony <Alint.Antony@gisbizdev.gisbiz.chennai.com>
2021-03-31 10:51:38 +0530

    Add project files.

---
+++ PaymentAPI/Repository/IPayRepos.cs
@@ -0,0 +1,17 @@
+﻿using PaymentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Repository
+{
+    public interface IPayRepos
+    {
+        IEnumerable<Payments> GetAllPayments();
+        Payments GetPaymentsbyId(int id);
+        Task<Payments> PostPayments(Payments item);
+        Task<Payments> UpdatePayment(Payments item, int id);
+
+    }
+}
---
+++ PaymentAPI/Repository/PayRepos.cs
@@ -0,0 +1,60 @@
+﻿using PaymentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Repository
+{
+    public class PayRepos : IPayRepos
+    {
+        private readonly CommunityGateDatabaseContext _context;
+
+        public PayRepos()
+        {
+
+        }
+
+        public PayRepos(CommunityGateDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Payments> GetAllPayments()
+        {
+            return _context.Payments.ToList();
+        }
+
+        public Payments GetPaymentsbyId(int id)
+        {
+            Payments item = _context.Payments.Find(id);
+            return item;
+        }
+
+        public async Task<Payments> PostPayments(Payments item)
+        {
+            Payments payment = null;
+            if (item == null)
+            {
+                throw new NullReferenceException();
+            }
+            else
+            {
+                payment = new Payments() { PaymentFor = item.PaymentFor,Amount = item.Amount, ResidentId = item.ResidentId, EmployeeId = item.EmployeeId,PaymentStatus = item.PaymentStatus, ServiceId = item.ServiceId};
+                await _context.Payments.AddAsync(payment);
+                await _context.SaveChangesAsync();
+            }
+            return payment;
+        }
+
+        public async Task<Payments> UpdatePayment(Payments item, int id)
+        {
+            Payments payment = await _context.Payments.FindAsync(id);
+            payment.PaymentStatus = item.PaymentStatus;
+            await _context.SaveChangesAsync();
+            return payment;
+        }
+
+
+    }
+}
